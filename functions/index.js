eec05d73f94d6640302f891d024b6079b6a6a412
MinMyatMaung <rickyminmyatmaung@gmail.com>
2025-05-18 03:36:03 -0700

    Edited index.js in functions

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,25 @@
 const functions = require('firebase-functions');
-const Filter = require('bad words');
-
+const Filter = require('bad-words');
 const admin = require('firebase-admin');
-admin.initializeApp();
 
+admin.initializeApp();
 const db = admin.firestore();
 
 exports.detectEvilUsers = functions.firestore
-    .document('messages/{msgId}')
-    .onCreate(async (doc, ctx)) => {
-        
-        const filter = new Filter();
-        const { text, uid } = doc.data();
+  .document('messages/{msgId}')
+  .onCreate(async (snap, ctx) => {
+    const filter = new Filter();
+    const { text, uid } = snap.data();
 
-        if (filter.isProfane(text)) {
-            const cleaned = filter.clean(text);
-            await doc.ref.update({text: 'I got banned for saying... ${cleaned}'});
+    if (filter.isProfane(text)) {
+      const cleaned = filter.clean(text);
+      
+      // Update the message text to a censored version
+      await snap.ref.update({
+        text: `I got banned for saying... ${cleaned}`
+      });
 
-            await db.collection('banned').doc(uid).set({});
-        }
+      // Add the user to a 'banned' collection
+      await db.collection('banned').doc(uid).set({});
     }
+  });
